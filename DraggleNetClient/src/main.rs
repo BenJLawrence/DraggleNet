ebb531f67de08228df1eac57150f1e844e7ee28d
Ben Lawrence <ben.lawrence0324@gmail.com>
2025-04-01 22:43:28 -0500

    Client/Server testing connection; Front end setup

--- DraggleNetClient/src/main.rs
+++ DraggleNetClient/src/main.rs
@@ -1,26 +1,56 @@
-use tonic::{Request, Response, Status};
-
-use crate::draggle_net::{PingRequest, PingResponse};
-use crate::draggle_net::draggle_net_client::DraggleNetClient;
-
 pub mod draggle_net {
     tonic::include_proto!("draggle_net");
 }
 
+use crate::draggle_net::draggle_net_client::DraggleNetClient;
+use crate::draggle_net::PingRequest;
+use dioxus::prelude::*;
+use tonic::Request;
 
-pub struct Client {
-
-}
+const FAVICON: Asset = asset!("/assets/favicon.ico");
+const MAIN_CSS: Asset = asset!("/assets/tailwind.css");
+const HEADER_SVG: Asset = asset!("/assets/header.svg");
 
 #[tokio::main]
-async fn main() -> Result<(), anyhow::Error> {
+async fn main() -> anyhow::Result<()> {
     let mut client = DraggleNetClient::connect("http://[::1]:50051").await?;
 
     //TODO make a function for into_request
-    let request = tonic::Request::new(PingRequest { message: "hello from the client".to_string() });
+    let request = Request::new(PingRequest { message: "ping from the client".to_string() });
 
     let response = client.send(request).await?;
     println!("Response: {:?}", response);
 
+
+    //TODO look at musa-gui launch_app. Probably will have to put the client event handling on a separate thread and it'll take the main thread
+    launch(App);
+
     Ok(())
+}
+
+#[component]
+fn App() -> Element {
+    rsx! {
+        document::Link { rel: "icon", href: FAVICON }
+        document::Link { rel: "stylesheet", href: MAIN_CSS }
+        Hero {}
+    }
+}
+
+#[component]
+pub fn Hero() -> Element {
+    rsx! {
+        div {
+            id: "hero",
+            img { src: HEADER_SVG, id: "header" }
+            div { id: "links",
+                a { href: "https://dioxuslabs.com/learn/0.6/", "📚 Learn Dioxus" }
+                a { href: "https://dioxuslabs.com/awesome", "🚀 Awesome Dioxus" }
+                a { href: "https://github.com/dioxus-community/", "📡 Community Libraries" }
+                a { href: "https://github.com/DioxusLabs/sdk", "⚙️ Dioxus Development Kit" }
+                a { href: "https://marketplace.visualstudio.com/items?itemName=DioxusLabs.dioxus", "💫 VSCode Extension" }
+                a { href: "https://discord.gg/XgGxMSkvUM", "👋 Community Discord" }
+            }
+        }
+    }
 }
--- DraggleNetServer/src/main.rs
+++ DraggleNetServer/src/main.rs
@@ -1,3 +1,39 @@
-fn main() {
-    println!("Hello, world!");
+pub mod draggle_net {
+    tonic::include_proto!("draggle_net");
+}
+
+use tonic::{async_trait, Request, Response, Status};
+use tonic::transport::Server;
+use draggle_net::*;
+use crate::draggle_net::draggle_net_server::{DraggleNet, DraggleNetServer};
+
+pub struct DraggleNetService {
+
+}
+
+#[async_trait]
+impl DraggleNet for DraggleNetService {
+    async fn send(&self, request: Request<PingRequest>) -> Result<Response<PingResponse>, Status> {
+        let message = request.into_inner();
+
+        println!("Got a ping: {:?}", message);
+
+        let reply = PingResponse {
+            message: "pong from server".to_string(),
+        };
+
+        Ok(Response::new(reply))
+    }
+}
+
+#[tokio::main]
+async fn main() -> anyhow::Result<()> {
+    let addr = "[::1]:50051".parse()?;
+    let service = DraggleNetService {};
+
+    println!("Server is listening on {}", addr);
+
+    Server::builder().add_service(DraggleNetServer::new(service)).serve(addr).await?;
+
+    Ok(())
 }
